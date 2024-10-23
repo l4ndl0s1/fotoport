7c20d6d43ea98f72272c23189e4de72e99ea0b8e
l4ndl0s1 <laurusedel@gmail.com>
2024-10-23 13:33:27 +0200

    header speak

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
+  @ViewChild('hoverSound') hoverSound!: ElementRef<HTMLAudioElement>;
 
-  
+  playSound(): void {
+    this.hoverSound.nativeElement.play();
+  }
+
+  stopSound(): void {
+    const audio = this.hoverSound.nativeElement;
+    audio.pause();
+  }
 }
