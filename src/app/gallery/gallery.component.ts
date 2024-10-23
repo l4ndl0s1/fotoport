fe2e096f4123a4e3ceb9e9de6cf4ee5b9c401e64
l4ndl0s1 <laurusedel@gmail.com>
2024-10-23 10:09:19 +0200

    crazy

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  ViewChildren,
-  ElementRef,
-  QueryList,
-} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ImageService } from '../images.service';
 import { Image } from '../models/images.module';
 
@@ -15,42 +9,50 @@ import { Image } from '../models/images.module';
 })
 export class GalleryComponent implements OnInit {
   images: Image[] = [];
-  displayLimit = 3;
-  allImagesLoaded = false;
+  displayLimit: number = 6;
   selectedImage: Image | null = null;
-  private lastLoadedIndex = 3;
-
-  @ViewChildren('imageContainer') imageContainers!: QueryList<ElementRef>;
-  @ViewChildren('loadMoreButton') loadMoreButton!: QueryList<ElementRef>;
-
+  @ViewChild('hoverSound') hoverSound!: ElementRef<HTMLAudioElement>;
+  @ViewChild('hoverSound1') hoverSound1!: ElementRef<HTMLAudioElement>;
   constructor(private imageService: ImageService) {}
 
   ngOnInit(): void {
     this.imageService.getImages().subscribe((data: Image[]) => {
       this.images = data;
-      this.lastLoadedIndex = this.displayLimit;
-      if (this.displayLimit >= this.images.length) {
-        this.allImagesLoaded = true;
-      }
     });
   }
 
-  loadMore() {
-    const increment = 1;
-    if (this.lastLoadedIndex + increment >= this.images.length) {
-      this.displayLimit = this.images.length;
-      this.allImagesLoaded = true;
-    } else {
-      this.displayLimit += increment;
-      this.lastLoadedIndex += increment;
-    }
+  loadMore(): void {
+    this.displayLimit = Math.min(this.displayLimit + 1, this.images.length);
+    this.hoverSound1.nativeElement.play();
   }
 
-  openImage(image: Image) {
+  get allImagesLoaded(): boolean {
+    return this.displayLimit >= this.images.length;
+  }
+
+  openImage(image: Image): void {
     this.selectedImage = image;
+    this.hoverSound.nativeElement.play();
   }
 
-  closeModal() {
+  closeModal(): void {
     this.selectedImage = null;
+  }
+  playSound(): void {
+    this.hoverSound.nativeElement.play();
+  }
+
+  stopSound(): void {
+    const audio = this.hoverSound.nativeElement;
+    audio.pause();
+  }
+
+  playSound1(): void {
+    this.hoverSound1.nativeElement.play();
+  }
+
+  stopSound1(): void {
+    const audio = this.hoverSound1.nativeElement;
+    audio.pause();
   }
 }
--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.css']
+  styleUrls: ['./testimonials.component.css'],
 })
 export class TestimonialsComponent {
-    // You can add properties or methods here if needed
+  // You can add properties or methods here if needed
 }
